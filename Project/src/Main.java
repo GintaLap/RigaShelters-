576d3e3cbc2bc6415a46093615dff1c36ebb15fd
Sanita Deksne <sanita.deksne@gmail.com>
2020-12-14 21:10:39 +0200

    Animal enter test

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -3,7 +3,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Animal catCharlie = new Animal("cat", "Charlie", 3, 30, 2, " Ulubele", false, 3256, false, false);
+        Animal catCharlie = new Animal("cat", "Charlie", 3, " large", 2, " Ulubele", false, 3256, false, false);
+        /*System.out.println(Animal);*/
+
     }
 
 }
