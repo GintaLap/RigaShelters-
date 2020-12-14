c5e3eaf9368ff1bb30c5f97a8c3cd661f55f7352
Sanita Deksne <sanita.deksne@gmail.com>
2020-12-14 20:55:07 +0200

    Getters and setters for Animal class

--- Project/src/Animal.java
+++ Project/src/Animal.java
@@ -9,4 +9,84 @@ public class Animal {
     private int id;
     private boolean isAdopted;
     private boolean isWalkable;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getAge() {
+        return age;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public float getTimeInShelter() {
+        return timeInShelter;
+    }
+
+    public String getAssignedShelter() {
+        return assignedShelter;
+    }
+
+    public boolean isNeedsSpecialTreatment() {
+        return needsSpecialTreatment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAdopted() {
+        return isAdopted;
+    }
+
+    public boolean isWalkable() {
+        return isWalkable;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(float age) {
+        this.age = age;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setTimeInShelter(float timeInShelter) {
+        this.timeInShelter = timeInShelter;
+    }
+
+    public void setAssignedShelter(String assignedShelter) {
+        this.assignedShelter = assignedShelter;
+    }
+
+    public void setNeedsSpecialTreatment(boolean needsSpecialTreatment) {
+        this.needsSpecialTreatment = needsSpecialTreatment;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAdopted(boolean adopted) {
+        isAdopted = adopted;
+    }
+
+    public void setWalkable(boolean walkable) {
+        isWalkable = walkable;
+    }
 }
