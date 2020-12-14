68c53b1dad319be45d90763df4adb899950cc97e
GintaLap <69529030+GintaLap@users.noreply.github.com>
2020-12-14 21:08:01 +0200

    Defined shellters

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,2 +1,12 @@
+
 public class Main {
+    public static void main(String[] args) {
+        Shelters ulubele = new Shelters("Ulubele", "Ozolaine", 20203333, "ulubele@ulubele.org",200, 87, "LV97HABA0551028353893");
+        Shelters dzīvniekuDraugs = new Shelters("Dzīvnieku Draugs", "Fridriha Candera iela 4", 67500491
+        , "DZD@LATNET.LV", 150, 100, "LV71HABA0551039535066");
+        Shelters labāsMājas = new Shelters("Labās Mājas", "Mežapurva iela 2", 26617636, "info@patversme.lv", 110,100, "LV95HABA0551003865018");
+    }
+
+
+
 }
