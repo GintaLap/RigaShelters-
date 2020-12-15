1b270ce464188e98c80fd8d26cd65021099c524d
Olga <egliteolga@gmail.com>
2020-12-15 20:56:43 +0200

    Added Date to expenses + method, which prints all expenses

--- Project/src/Expense.java
+++ Project/src/Expense.java
@@ -1,4 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Expense {
+
+    private LocalDateTime localDateTime;
 
     private String purpose; // food, electricity, salary, medicalTreatment (animal ID. pass as attribute somehow.)
     private double amount;
@@ -6,7 +12,20 @@ public class Expense {
     public Expense(String purpose, double amount) {
         this.purpose = purpose;
         this.amount = amount;
+        this.localDateTime = LocalDateTime.now();
     }
 
+    public String getPurpose() {
+        return purpose;
+    }
 
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getLocalDateTime() { // getting local time, when method was called
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:m", Locale.ENGLISH);
+        String formatted = dateFormat.format(localDateTime);
+        return formatted;
+    }
 }
