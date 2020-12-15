9308c2a7da199a18203a34316bdfecffae77e911
GintaLap <69529030+GintaLap@users.noreply.github.com>
2020-12-15 18:49:04 +0200

    Created Animal Registration in text file.

---
+++ Project/src/AnimalsRegistration.java
@@ -0,0 +1,44 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class AnimalsRegistration {
+
+
+    public static class AnimalRegistration {
+        public AnimalRegistration() {
+        }
+
+        public static void main(String[] args) throws IOException {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Dzīvnieku reģistrs");
+            System.out.println("Dzīvnieka vārds: ");
+            String name = scan.nextLine();
+            System.out.println("Dzīvnieka tips(suns, ķaķis, cits): ");
+            String type = scan.nextLine();
+            System.out.println("Dzīvnieka vecums: ");
+            int age = scan.nextInt();
+            System.out.println("Dzīvnieka lielums (mazs, vidējs, liels): ");
+            String size = scan.next();
+            System.out.println("Pavadītais laiks patversmē (nedēļas): ");
+            int timeInShelter = scan.nextInt();
+            System.out.println("Patversme(ulubele, dzīvnieku draugs, labās mājas): ");
+            String assignedShelter = scan.next();
+            System.out.println("Īpašas vajadzības: ");
+            boolean needsSpecialTreatment = scan.nextBoolean();
+            System.out.println("Adoptēts: ");
+            boolean isAdopted = scan.nextBoolean();
+            System.out.println("Piemērots brīvprātīgajiem: ");
+            boolean isWalkable = scan.nextBoolean();
+            System.out.println("ID numurs: ");
+            int id = scan.nextInt();
+            FileWriter myFile = new FileWriter("Dzīvnieku_reģistrs.txt", true);
+            PrintWriter writeIntoFile = new PrintWriter(myFile);
+            writeIntoFile.printf("%s %s %s %s %s %s %s %s %s \n", name, type, age, size, timeInShelter, assignedShelter, needsSpecialTreatment, isAdopted, isWalkable, id);
+            myFile.close();
+            System.out.println("Dzīvnieks ir reģistrēts!");
+            scan.close();
+        }
+    }
+}
