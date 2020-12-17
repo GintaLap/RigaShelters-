72578d2af7b9105cf05eb8e859814b2c69505838
GintaLap <69529030+GintaLap@users.noreply.github.com>
2020-12-17 21:27:11 +0200

    added shelterId in Shelter class, defined it in Main

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,10 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        Shelters ulubele = new Shelters("Ulubele", "Ozolaine", 20203333, "ulubele@ulubele.org", 200, 87, "LV97HABA0551028353893");
+        Shelters ulubele = new Shelters("Ulubele", "Ozolaine", 20203333, "ulubele@ulubele.org", 200, 87, "LV97HABA0551028353893",1 );
         Shelters dzīvniekuDraugs = new Shelters("Dzīvnieku Draugs", "Fridriha Candera iela 4", 67500491
-                , "DZD@LATNET.LV", 150, 100, "LV71HABA0551039535066");
-        Shelters labāsMājas = new Shelters("Labās Mājas", "Mežapurva iela 2", 26617636, "info@patversme.lv", 110, 100, "LV95HABA0551003865018");
+                , "DZD@LATNET.LV", 150, 100, "LV71HABA0551039535066", 1);
+        Shelters labāsMājas = new Shelters("Labās Mājas", "Mežapurva iela 2", 26617636, "info@patversme.lv", 110, 100, "LV95HABA0551003865018", 1);
 
 
         BankAccount ulubeleAccount = new BankAccount("LV97HABA0551028353893", 2000.0);
