c1fd0d8b7c87201268966144ba0857530b2771be
Olga <egliteolga@gmail.com>
2020-12-17 22:50:39 +0200

    Initial commit of Volunteer class

---
+++ Project/src/Volunteer.java
@@ -0,0 +1,7 @@
+public class Volunteer extends Person{
+    // id number?
+    public Volunteer(String personName, String personSurname, String phoneNumber) {
+        super(personName, personSurname, phoneNumber);
+    }
+    // assign Volunteer to animal that needs walking
+}
