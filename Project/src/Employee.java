6f3a29d408e4a2f0d60d1b0849006c7e7fd13eb2
Olga <egliteolga@gmail.com>
2020-12-21 22:09:06 +0200

    Added employee count, shelter assignment

--- Project/src/Employee.java
+++ Project/src/Employee.java
@@ -1,6 +1,4 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class Employee extends Person{
@@ -8,19 +6,22 @@ public class Employee extends Person{
     private String position;
     private String bankAccountDetails;
     private double salary;
+    public static int employeeCountU = 0;
+    public static int employeeCountUDD = 0;
+    public static int employeeCountLM = 0;
 
     public Employee(String personName, String personSurname, String phoneNumber, String bankAccountDetails, String position) {
         super(personName, personSurname, phoneNumber);
         this.bankAccountDetails = bankAccountDetails;
         this.position = position;
         if (position.equalsIgnoreCase("manager")){
-            this.salary = 1500;
+            this.salary = 0;
         }
         else if (position.equalsIgnoreCase("worker")){
-            this.salary = 2000;
+            this.salary = 0;
         }
         else
-            this.salary = 1000;
+            this.salary = 0;
 
     }
     public static void employeeRegistration() {
@@ -34,26 +35,71 @@ public class Employee extends Person{
         String bankAccountDetails = scan.nextLine();
         System.out.println("Employee's job position: ");
         String position = scan.nextLine();
+
+        String[] jobPosition = new String[]{"worker", "manager", "cleaner"};
+        System.out.println("Job position:");
+        for (byte i = 0; i < jobPosition.length; i++) {
+            System.out.println(i + 1 + " - " + jobPosition[i]);
+        }
+        System.out.print("Enter number accordingly: ");
+        byte j = scan.nextByte();
+        while (j <= 0 || j > 3) {
+            System.out.print("Incorrect entry. Please enter number accordingly (1-3): ");
+            j = scan.nextByte();
+        }
+
+        String[] employeeAssignedShelter = new String[]{"Ulubele", "Dzīvnieku_Draugs", "Labās_Mājas"};
+        System.out.println("Please choose assigned Shelter:");
+        for (byte i = 0; i < employeeAssignedShelter.length; i++) {
+            System.out.println(i + 1 + " - " + employeeAssignedShelter[i]);
+        }
+        System.out.print("Enter number accordingly: ");
+        byte s = scan.nextByte();
+        while (s <= 0 || s > 3) {
+            System.out.print("Incorrect entry. Please enter number accordingly (1-3): ");
+            s = scan.nextByte();
+        }
         System.out.println("Employee's phone number: ");
         int phoneNumber = scan.nextInt();
 
-        String employeeRegistration = "Employee_Registration";
+        System.out.println("Employee_Registration");
         try (FileWriter fileWriter = new FileWriter("Employee_Registration", true)) {
             PrintWriter writeIntoFile = new PrintWriter(fileWriter);
-            writeIntoFile.printf("%s %s %s %s %s \n", personName, personSurname, phoneNumber, bankAccountDetails, position);
+            writeIntoFile.printf("%s %s %s %s %s %s\n", personName, personSurname, phoneNumber, bankAccountDetails, employeeAssignedShelter[s - 1], jobPosition [j - 1]);
             fileWriter.close();
             System.out.println("Employee is registered!");
         } catch (IOException e) {
             System.out.println("Connection failed!");
         }
     }
+    public  static void countEmployees () {
+        try {
+            File myObj = new File("Employee_Registration");
+            Scanner employeeReader = new Scanner(myObj);
+            while (employeeReader.hasNextLine()) {
+                String employeeData = employeeReader.nextLine();
+                String[] employeeFields = employeeData.split(" ");
+                if (employeeFields [4].equals("Ulubele"))
+                    employeeCountU++;
+                if (employeeFields [4].equals("Dzīvnieku_Draugs"))
+                    employeeCountUDD++;
+                if (employeeFields[4].equals("Labās_Mājas"))
+                    employeeCountLM++;
+            }
+            employeeReader.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
 
     public double paySalary(BankAccount shelterAccount){
         double balance = shelterAccount.getBalance();
          balance -= this.salary;
         System.out.println("Salary for " + super.getPersonName() + " " + super.getPersonSurname() + " : " + this.salary +
                 " . Remaining balance: " + balance + " EUR. ");
-        // try to add to expense
         shelterAccount.addExpense("salary",this.salary );
         return balance;
     }
